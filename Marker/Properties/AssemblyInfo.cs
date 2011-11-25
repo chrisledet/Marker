078ff1745f1180c9175cad0973e895278bc8e220
Chris Ledet <chris@ledet.io>
2011-11-24 23:18:54 -0500

    update to v0.0.4

--- Marker/Properties/AssemblyInfo.cs
+++ Marker/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Marker")]
-[assembly: AssemblyCopyright("Copyright Chris Ledet ©  2011")]
+[assembly: AssemblyCopyright("Copyright © 2011 Chris Ledet")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.3.0")]
-[assembly: AssemblyFileVersion("0.0.3.0")]
+[assembly: AssemblyVersion("0.0.4.0")]
+[assembly: AssemblyFileVersion("0.0.4.0")]
