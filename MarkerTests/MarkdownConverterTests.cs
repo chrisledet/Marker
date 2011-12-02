76afc04bcaca26c083b6a57131c461081d530f71
Chris Ledet <chris@ledet.io>
2011-12-01 22:41:18 -0500

    Added Unit Tests for Marker. Relates to #11.

---
+++ MarkerTests/MarkdownConverterTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using Marker;
+using System;
+
+namespace MarkerTests
+{
+    public class MarkdownConverterTests
+    {
+        private MarkdownConverter converter;
+
+        public MarkdownConverterTests()
+        {
+            converter = new MarkdownConverter();
+        }
+
+        [Fact]
+        public void TestMe()
+        {
+            String markdownText = "### Hello World";
+            String htmlText = "<h3>Hello World</h3>";
+            Assert.True( converter.convert(markdownText).Contains(htmlText) );
+        }
+    }
+}
---
+++ MarkerTestz/MarkdownConverterTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Marker;
+
+namespace MarkerTests
+{
+    [TestClass]
+    public class MarkdownConverterTest
+    {
+        private MarkdownConverter converter;
+
+        public MarkdownConverterTest()
+        {
+            converter = new MarkdownConverter();
+        }
+
+        [TestMethod]
+        public void TestConvert()
+        {
+            String convertedMarkdown = converter.convert("### Hello World");
+            String htmlText = "<h3>Hello World</h3>";
+            Assert.IsTrue( convertedMarkdown.Contains(htmlText) );
+        }
+
+        [TestMethod]
+        public void TestFont()
+        {
+            String font = "Arial";
+            converter.Font = font;
+            Assert.AreEqual(font, converter.Font);
+        }
+    }
+}
